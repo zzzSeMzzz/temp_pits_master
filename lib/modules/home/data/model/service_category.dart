052ea64c782ebe9c992ff683d83ef4d383f89fdf
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-07-09 15:44:02 +0300

    service category

---
+++ lib/modules/home/data/model/service_category.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+import '../../domain/entity/service_category.dart';
+part 'service_category.g.dart';
+
+@JsonSerializable()
+class ServiceCategoryModel extends ServiceCategory {
+
+  @JsonKey(name: 'id', defaultValue: -1)
+  final int idd;
+  @JsonKey(name: 'name', defaultValue: '')
+  final String nameD;
+
+  ServiceCategoryModel({
+    required this.idd,
+    required this.nameD,
+  }) : super(
+    id: idd,
+    name: nameD,
+  );
+
+}
---
+++ lib/modules/home/data/model/service_category.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'service_category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ServiceCategoryModel _$ServiceCategoryModelFromJson(
+        Map<String, dynamic> json) =>
+    ServiceCategoryModel(
+      idd: (json['id'] as num?)?.toInt() ?? -1,
+      nameD: json['name'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$ServiceCategoryModelToJson(
+        ServiceCategoryModel instance) =>
+    <String, dynamic>{
+      'id': instance.idd,
+      'name': instance.nameD,
+    };
