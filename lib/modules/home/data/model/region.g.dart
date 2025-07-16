e3f847541dec19862708e9ff0d4af2ec270b0465
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-07-16 11:10:56 +0300

    fetch regions

---
+++ lib/modules/home/data/model/region.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'region.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RegionModel _$RegionModelFromJson(Map<String, dynamic> json) => RegionModel(
+      id: (json['id'] as num?)?.toInt() ?? -1,
+      name: json['name'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$RegionModelToJson(RegionModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
