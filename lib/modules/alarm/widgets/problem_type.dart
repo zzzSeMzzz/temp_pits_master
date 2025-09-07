8d6547e4081098c4252d3ca037a4066787d9422c
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-09-07 09:30:10 +0300

    on alarm select

--- lib/modules/alarm/widgets/problem_type.dart
+++ lib/modules/alarm/widgets/problem_type.dart
@@ -9,7 +9,6 @@ class ProblemType extends StatelessWidget {
     required this.name,
     required this.image,
     required this.isSelected,
-    //required this.onTap,
     required this.onSelectAlarm,
   });
 
