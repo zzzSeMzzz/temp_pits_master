439a2c0d612a59089fd9be2903019573eee98cab
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-10-04 12:12:04 +0300

    add auth interceptor

--- lib/core/data/interceptors/auth_interceptor.dart
+++ lib/core/data/interceptors/auth_interceptor.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pits_app/core/data/singletons/storage.dart';
 import 'package:pits_app/modules/auth/presentation/sections/auth_screen/auth_screen.dart';
+import 'package:pits_app/utils/utils.dart';
 
 class AuthInterceptor extends Interceptor {
   static final GlobalKey<NavigatorState> navigatorKey =
@@ -9,10 +10,10 @@ class AuthInterceptor extends Interceptor {
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) async {
-    print('AuthInterceptor: Response status code: ${response.statusCode}');
+    debugPrint('AuthInterceptor: Response status code: ${response.statusCode}');
 
     if (response.statusCode == 401) {
-      print('AuthInterceptor: 401 detected, logging out and redirecting');
+      debugPrint('AuthInterceptor: 401 detected, logging out and redirecting');
 
       // Сбрасываем авторизацию
       StorageRepository.logout();
@@ -20,14 +21,21 @@ class AuthInterceptor extends Interceptor {
       // Получаем текущий контекст и перенаправляем на экран авторизации
       final context = navigatorKey.currentContext;
       if (context != null) {
-        print('AuthInterceptor: Context found, redirecting to auth screen');
+        debugPrint('AuthInterceptor: Context found, redirecting to auth screen');
+
+        // Показываем уведомление об истечении сессии
+        Utils.flushBarErrorMessage(
+          'Su sesión ha expirado. Por favor, inicie sesión nuevamente.',
+          context,
+        );
+
         // Очищаем весь стек навигации и переходим на экран авторизации
         Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) => const AuthScreen()),
           (route) => false,
         );
       } else {
-        print('AuthInterceptor: No context found');
+        debugPrint('AuthInterceptor: No context found');
       }
 
       // Выбрасываем исключение, чтобы репозиторий получил ошибку
@@ -55,6 +63,12 @@ class AuthInterceptor extends Interceptor {
       // Получаем текущий контекст и перенаправляем на экран авторизации
       final context = navigatorKey.currentContext;
       if (context != null) {
+        // Показываем уведомление об истечении сессии
+        Utils.flushBarErrorMessage(
+          'Su sesión ha expirado. Por favor, inicie sesión nuevamente.',
+          context,
+        );
+
         // Очищаем весь стек навигации и переходим на экран авторизации
         Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) => const AuthScreen()),
