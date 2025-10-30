0717ef2254656ea3eb1cb5b93cb759868b50d680
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-10-30 16:10:28 +0300

    service repair icons

--- lib/modules/service/presentation/sections/service_selection/service_selection_screen.dart
+++ lib/modules/service/presentation/sections/service_selection/service_selection_screen.dart
@@ -15,69 +15,84 @@ class ServiceSelectionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: white,
-        body: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 13 + MediaQuery.of(context).padding.top,
-              ),
-              GestureDetector(
-                  behavior: HitTestBehavior.translucent,
+        body: SafeArea(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 13,
+                ),
+                GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: SvgPicture.asset(
+                      AppIcons.arrowLeft,
+                      width: 24,
+                      height: 24,
+                      color: mainDark,
+                    )),
+                const SizedBox(
+                  height: 35,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Servicios (multiselección)',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!
+                          .copyWith(fontSize: 24, fontWeight: FontWeight.w700),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Expanded(
+                  child: GridView.builder(
+                      itemCount: AppIcons.serviceRepairIcons.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisExtent: 160,
+                              childAspectRatio: 1,
+                              mainAxisSpacing: 8,
+                              crossAxisSpacing: 8),
+                      itemBuilder: (context, index) {
+                        final repairInfo = AppIcons.serviceRepairIcons.entries.elementAt(index);
+                        return SelectionBox(
+                            onTap: () {
+
+                            },
+                            title: repairInfo.key,
+                            svgIcon: repairInfo.value
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: 20
+                ),
+                WButton(
                   onTap: () {
-                    Navigator.pop(context);
+                    Navigator.of(context, rootNavigator: true)
+                        .push(fade(page: const RepairSelectionScreen()));
                   },
-                  child: SvgPicture.asset(
-                    AppIcons.arrowLeft,
-                    width: 24,
-                    height: 24,
-                    color: mainDark,
-                  )),
-              const SizedBox(
-                height: 35,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Servicios (multiselección)',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .displayLarge!
-                        .copyWith(fontSize: 24, fontWeight: FontWeight.w700),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 32,
-              ),
-              Expanded(
-                child: GridView.builder(
-                    itemCount: AppIcons.serviceRepairIcons.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            mainAxisExtent: 160,
-                            childAspectRatio: 1,
-                            mainAxisSpacing: 8,
-                            crossAxisSpacing: 8),
-                    itemBuilder: (context, index) {
-                      final repairInfo = AppIcons.serviceRepairIcons.entries.elementAt(index);
-                      return SelectionBox(
-                          onTap: () {
-                            Navigator.push(context, fade(page: const RepairSelectionScreen()));
-                          },
-                          title: repairInfo.key,
-                          svgIcon: repairInfo.value
-                      );
-                    }),
-              ),
-              SizedBox(
-                height: 30 + MediaQuery.of(context).padding.bottom,
-              ),
-            ],
+                  height: 55,
+                  borderRadius: 4,
+                  textColor: white,
+                  text: 'Continue',
+                ),
+                const SizedBox(
+                    height: 12
+                ),
+              ],
+            ),
           ),
         ),
       );
